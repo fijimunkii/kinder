0646eef11a7f0f6a98db963abe02c7418e0fb8c2
Harrison Powers <fijimunkii@gmail.com>
2013-12-06 15:03:54 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kinder::Application.config.secret_token = 'c14740fcb55ab32d254aea80e47081a04d16b0232aa6c27e63328cbf986417bd7c32c0e8c12aa52d166019257639a91dff7430eaeff574d52c82ee62462b24b2'
